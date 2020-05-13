7ea7772dc89bcab7d374e822d56c49126bb670c2
kakeru <okinawan.kkr0713@icloud.com>
2020-05-14 06:16:26 +0900

    step4完了

---
+++ ch11/HighAndLowS04.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+    <center>
+       
+    <h1>High & Lowゲーム</h1>
+    <hr>
+        <?php
+        echo "<p>選択カード番号：{$_POST['leftCard']}</p>";
+            echo "<p>High&Low選択：{$_POST['select']}</p>";
+        
+        ?>
+    </center>
+</body>
+
+</html>

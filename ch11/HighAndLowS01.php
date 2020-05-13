08b1caa1cd1cae33bb08d5a78bfe176697fff92d
kakeru <okinawan.kkr0713@icloud.com>
2020-05-13 19:55:47 +0900

    step1kannryou

---
+++ ch11/HighAndLowS01.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+    <center>
+    <h1>High & Lowゲーム</h1>
+    <hr>
+    <?php
+        $leftCard = mt_rand(1,13);
+        echo "ランダム数値：{$leftCard}";
+    ?>
+    </center>
+</body>
+
+</html>

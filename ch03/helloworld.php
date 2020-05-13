0ffb81d789a76aaa2a273b8a2b551a6db4a745c1
kakeru <okinawan.kkr0713@icloud.com>
2020-05-13 19:36:39 +0900

    fix

--- ch03/helloworld.php
+++ ch03/helloworld.php
@@ -1,7 +1,10 @@
 <html>
   <body>
     <?php
-    echo "hello world";
-     ?>
+        echo "hello world";
+        echo "<br>";
+        echo '<img src="../cards/07.png" >';
+    ?>
   </body>
 </html>
+        

314f7c7f7db727375f9d37b4ba24fa4fe7ee042a
kakeru <okinawan.kkr0713@icloud.com>
2020-05-14 07:40:36 +0900

    step6完了

--- ch11/HighAndLowS03.php
+++ ch11/HighAndLowS03.php
@@ -9,7 +9,7 @@
        
     <h1>High & Lowゲーム</h1>
     <hr>
-        <form action="highAndLowS05.php" method="post">
+        <form action="highAndLowS06.php" method="post">
             <?php
         //        カード配列
                 $cards = array("jk.png", "01.png", "02.png", "03.png", "04.png", "05.png", "06.png", "07.png", "08.png", "09.png", "10.png", "11.png", "12.png", "13.png");
